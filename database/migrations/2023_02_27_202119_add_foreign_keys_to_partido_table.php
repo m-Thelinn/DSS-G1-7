ed3bfd39ef0095973904b7b7f472b7cf3a6975d0
Miguel Rodriguez <mrs121@alu.ua.es>
2023-02-27 22:16:12 +0100

    Seeders corregidos y base de datos cambiada

--- database/migrations/2023_02_27_202119_add_foreign_keys_to_partido_table.php
+++ database/migrations/2023_02_27_202119_add_foreign_keys_to_partido_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('partido', function (Blueprint $table) {
-            $table->foreign(['equipo1'], 'equipo1')->references(['id'])->on('equipo')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign(['equipo2'], 'equipo2')->references(['id'])->on('equipo')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+            $table->foreign(['equipo_local'], 'partido_equipo_id_equipo_fk')->references(['id_equipo'])->on('equipo')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+            $table->foreign(['equipo_visitante'], 'partido_equipo_id_equipo_fk2')->references(['id_equipo'])->on('equipo')->onUpdate('NO ACTION')->onDelete('NO ACTION');
         });
     }
 
@@ -27,8 +27,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('partido', function (Blueprint $table) {
-            $table->dropForeign('equipo1');
-            $table->dropForeign('equipo2');
+            $table->dropForeign('partido_equipo_id_equipo_fk');
+            $table->dropForeign('partido_equipo_id_equipo_fk2');
         });
     }
 };
