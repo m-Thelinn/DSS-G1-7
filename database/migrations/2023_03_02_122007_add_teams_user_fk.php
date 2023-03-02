4b3ea5c491e24577801c107b3b44d2fa870712c7
Francisco  Fernandez Ruiz <ffr9@alu.ua.es>
2023-03-02 17:36:37 +0100

    Seeders creados, faltan FK equipo-partido

--- database/migrations/2023_03_02_122007_add_teams_user_fk.php
+++ database/migrations/2023_03_02_122007_add_teams_user_fk.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table){
-            $table->foreignId('team_id')->constrained();
+            $table->foreignId('team_id')->constrained();            
         });
     }
 
