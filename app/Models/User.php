67b2bce66da9c1e3581abf9c2665f420dd8cd5b9
Francisco  Fernandez Ruiz <ffr9@alu.ua.es>
2023-03-31 20:59:41 +0200

    cambio userstat.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
         return $this->belongsTo(Team::class);
     }
 
-    public function teamstat(){
+    public function userstat(){
         return $this->hasOne(Userstat::class);
     }
 }
