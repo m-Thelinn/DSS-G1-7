f5783a85f3a2282cdb49b39ae7be59801f419f24
ffr9 <pacofernandez2001@gmail.com>
2023-04-25 20:33:45 +0200

    login registro y cambios en models, rutas, ...

--- resources/views/homeBlade.blade.php
+++ resources/views/homeBlade.blade.php
@@ -3,6 +3,18 @@
         <title>Home</title>
     </head>
     <body>
+        <!-- aqui cambiar para hacer vista de usuario sin registrar
+        añadir navbar con botones de registro e inicio sesion
+        cuando clicas boton redirigir ruta de registro o inicio sesion (todas las comprobaciones) 
+        y despues redirigir home de registrado normal o de admin
+        -->
+
+        <!--NAVBAR-->
+        <a href="{{ route('login') }}"><input type="button" value="INICIO SESION"></a>
+        <a href="{{ route('register') }}"><input type="button" value="REGISTRO"></a>
+
+
+        <!--esta es la parte de home de admin-->
         <h1>Seleccione una opcion para ver los datos</h1>
         
         <a href="{{ route('user.showAllUsers') }}"><input type="button" value="Usuarios"></a>
