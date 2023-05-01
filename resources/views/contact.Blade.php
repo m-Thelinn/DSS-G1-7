61f4f9e0e2ff88da5966eebbe8b2c7e9cee3ccaa
ffr9 <pacofernandez2001@gmail.com>
2023-05-01 12:50:36 +0200

    about contact y rutas modificadas

--- resources/views/contact.Blade.php
+++ resources/views/contact.Blade.php
@@ -4,9 +4,29 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
-
             <h1>CONTACTO</h1>
+            <br>
+
+            <div>
+                <p>FRANCISCO FERNÁNDEZ RUIZ</p>
+                <p>ffr9@gcloud.ua.es</p>
+            </div>
+            <br>
+            <div>
+                <p>MIGUEL RODRÍGUEZ SÁNCHEZ</p>
+                <p>mrs121@gcloud.ua.es</p>
+            </div>
+            <br>
+            <div>
+                <p>DEMIAN CALVO RODRÍGUEZ</p>
+                <p>dcr28@gcloud.ua.es</p>
+            </div>
+            <br>
+            <div>
+                <p>JULIO CÉSAR BASTIDAS SÁNCHEZ</p>
+                <p>jcbs6@gcloud.ua.es</p>
+            </div>
+            <br>            
         </div>
     </div>
 </div>
