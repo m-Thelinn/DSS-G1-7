38222f27f03c30402e4122dcd34c5afd8e09be6d
Julio <jcbs6@alu.ua.es>
2023-05-13 11:05:33 +0200

    Pagina Contacto

--- resources/views/contact.Blade.php
+++ resources/views/contact.Blade.php
@@ -1,33 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<section class="contact-bg mb-50" id="home">
+    <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8 text">
-            <h1>CONTACTO</h1>
-            <br>
+        <h1 class="home-title mb-2">EQUIPO</h1>
+        <p class="contact-subtitle mb-5">Sobre nosotros</p>
+            <p>Somos un equipo de cobertura de Esports de Valorant apasionado por proporcionar información precisa 
+                y actualizada sobre calendarios, resultados de partidos, clasificaciones de equipos y estadísticas detalladas. 
+                Nos enorgullece ser el mejor recurso para la comunidad de Valorant y mantener a los fanáticos actualizados sobre 
+                los últimos acontecimientos en el mundo de los Esports de Valorant.</p>
 
             <div>
-                <p>FRANCISCO FERNÁNDEZ RUIZ</p>
-                <p>ffr9@gcloud.ua.es</p>
+                <br>
+                @guest
+                    <a class="btn-default" href="{{ route('register') }}">Unirse a nosotros</a>
+                @else
+                    <a class="btn-default" href="#options">Empezar a explorar</a>
+                @endguest
             </div>
-            <br>
+</section>
+
+<section>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 text">
             <div>
+                <img class="contact-img" src="/img/miguel.png" alt="">
+                <p class="options-title">PRESIDENTE</p>
                 <p>MIGUEL RODRÍGUEZ SÁNCHEZ</p>
-                <p>mrs121@gcloud.ua.es</p>
+                <p class="contact-email-text"><a href="mailto:jcbs6@gcloud.ua.es">mrs121@gcloud.ua.es</a></p>
+                <p>
+                    <a href="https://www.instagram.com/miguelrs_02/">
+                        <img class="contact-img-rrss" img src="/img/instagram.png" alt="Instagram">
+                    </a>
+                    <a href="https://twitter.com/th3linn/">
+                        <img class="contact-img-rrss" img src="/img/twitter.png" alt="Twitter">
+                    </a>
+                </p>
             </div>
             <br>
             <div>
+                <img class="contact-img" src="/img/paco.JPG" alt="">
+                <p class="options-title">VICEPRESIDENTE</p>
+                <p>FRANCISCO FERNÁNDEZ RUIZ</p>
+                <p class="contact-email-text"><a href="mailto:jcbs6@gcloud.ua.es">ffr9@gcloud.ua.es</a></p>
+                <p>
+                    <a href="https://www.instagram.com//">
+                        <img class="contact-img-rrss" img src="/img/instagram.png" alt="Instagram">
+                    </a>
+                    <a href="https://twitter.com//">
+                        <img class="contact-img-rrss" img src="/img/twitter.png" alt="Twitter">
+                    </a>
+                </p>
+            </div>
+            <br>
+            <div>
+                <img class="contact-img" src="/img/demian.png" alt="">
+                <p class="options-title">ANALISTA DE DATOS</p>
                 <p>DEMIAN CALVO RODRÍGUEZ</p>
-                <p>dcr28@gcloud.ua.es</p>
+                <p class="contact-email-text"><a href="mailto:jcbs6@gcloud.ua.es">dcr28@gcloud.ua.es</a></p>
+                <p>
+                    <a href="https://www.instagram.com/demiancr12/">
+                        <img class="contact-img-rrss" img src="/img/instagram.png" alt="Instagram">
+                    </a>
+                    <a href="https://twitter.com/demiancr_12/">
+                        <img class="contact-img-rrss" img src="/img/twitter.png" alt="Twitter">
+                    </a>
+                </p>
             </div>
             <br>
             <div>
+                <img class="contact-img" src="/img/julio.jpeg" alt="">
+                <p class="options-title">GERENTE DE MARKETING</p>
                 <p>JULIO CÉSAR BASTIDAS SÁNCHEZ</p>
-                <p>jcbs6@gcloud.ua.es</p>
+                <p class="contact-email-text"><a href="mailto:jcbs6@gcloud.ua.es">jcbs6@gcloud.ua.es</a></p>
+                <p>
+                    <a href="https://www.instagram.com/juliobastidas11/">
+                        <img class="contact-img-rrss" img src="/img/instagram.png" alt="Instagram">
+                    </a>
+                    <a href="https://twitter.com/juliobastidass/">
+                     <img class="contact-img-rrss" img src="/img/twitter.png" alt="Twitter">
+                    </a>
+                </p>
             </div>
             <br>            
         </div>
     </div>
 </div>
+</section>
 @endsection
