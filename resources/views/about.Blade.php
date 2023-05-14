a22a55c5e86d183f2bd5d8085b4cc51673d1482b
dcr28 <dcr28@alu.ua.es>
2023-05-14 23:27:42 +0200

    Pagina Informacion

--- resources/views/about.Blade.php
+++ resources/views/about.Blade.php
@@ -1,20 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 text">
-            <h1>INFORMACION DEL PROYECTO</h1>
-            <br>
-            <p>Nos enfocamos en cubrir noticias, estadísticas y eventos del juego llamado Valorant.</p>
-            <br>
-            <p>Ofrecemos información detallada sobre equipos y jugadores profesionales y torneos importantes de todo el mundo.</p>
-            <br>
-            <p>Proporcionamos datos de las partidas, clasificaciones del juego y una seccion de análisis y estadísticas.</p>
-            <br>
-            <p>Valcheck es una herramienta esencial para aquellos que desean mantenerse al día con el mundo competitivo de Valorant.</p>
-            <br>
+<section class="about-bg mb-50" id="home">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text">
+            <h1 class="about-title mb-2">THE MOST ENTERTAINING ESPORTS COVERAGE IN THE WORLD</h1>
+            </div>
         </div>
     </div>
-</div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row justify-content">
+            <div class="col-md-8 text">
+
+            <h2 class="left"> EQUIPOS</h2>
+            <div class="imagen-equipos">
+                <img src="img/equipos.jpg" width="250">
+            </div>
+
+            <h2 class="center"> JUGADORES</h2>
+            <div class="imagen-jugadores">
+                <img src= "img/jugador2.jpeg" width="285">
+            </div>
+
+            <h2 class="right"> PARTIDOS</h2>
+            <div class="imagen-partidos">
+                <img src= "img/partidos.jpg" width="285">
+            </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+            <div class="row justify-content">
+                <div class="col-md-8 text">
+                <p class="parrafo-izquierda">En nuestra pagina web tendremos la opcion
+                <br> de poder observar todos los equipos, asi como las estadisticas
+                <br> de los mismos.
+                </p>
+                </div>
+
+                <p class="parrafo-medio"> Además de los equipos, podremos ver
+                <br>los jugadores que componen cada equipo
+                <br> y las estadisticas de ellos respecto
+                <br> a los partidos jugados.
+                </p>
+
+                <p class="parrafo-derecha"> Por último tenemos la posibilidad de 
+                <br> ver los resultados de los partidos que se
+                <br> han disputado.
+                </p>
+            </div>
+    </div>
+</section>
+
 @endsection
