759078cdbbc0f14625d45a5271a49e14d3760647
ffr9 <pacofernandez2001@gmail.com>
2023-05-22 11:41:32 +0200

    modificar perfil arreglado

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,34 +22,29 @@ class ProfileController extends Controller
     }
 
     public function updateProfile(){ 
-        //dd("llega");
+        
         request()->validate([
             'name' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
             'nickname' => 'required|string|max:255',
             'email' => 'required|email',
-            'password' => [
-                'required',
-                'min:8',
-            ],
             'country' => 'required|string|max:255',
             'phone' => 'required|string|max:255',
             'rol' => 'required|integer|between:0,3',
             'team_id' => 'required|string|max:255',
         ]);
+        
         $user = Auth::user();
         $team = Team::select()->where('name', request('team_id'))->first();
         $user->name = request('name');
         $user->lastname = request('lastname');
         $user->nickname = request('nickname');
         $user->email = request('email');
-        $user->password = request('password');
         $user->phone = request('phone');
         $user->rol = request('rol');
         $user->country = request('country');
         $user->team_id = $team->id;
         $user->save();
-        
         return redirect('/profile');
     }
 }
